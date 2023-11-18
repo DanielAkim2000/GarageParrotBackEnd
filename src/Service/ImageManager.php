645608e8786e4f33cf7e20eb58896d783722fbc0
DanielAkim2000 <emanedanielakim@gmail.com>
2023-11-18 21:40:22 +0100

    Fix bug image aws S3

--- src/Service/ImageManager.php
+++ src/Service/ImageManager.php
@@ -4,9 +4,9 @@
 
 namespace App\Service;
 
+use Aws\S3\Exception\S3Exception;
 use Aws\S3\S3Client;
 use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class ImageManager
@@ -24,18 +24,16 @@ class ImageManager
     {
         $bucketName = $this->parameterBag->get('app.s3_bucket');
         $uploadDirectory = $this->parameterBag->get('app.upload_directory');
-
+    
         $fileName = uniqid().'.'.$file->guessExtension();
-        $file->move($uploadDirectory, $fileName);
-
+    
         // Utiliser le client AWS S3 pour uploader le fichier dans le compartiment S3
         $this->s3Client->putObject([
             'Bucket' => $bucketName,
             'Key' => $uploadDirectory.'/'.$fileName,
             'Body' => fopen($file->getPathname(), 'r'),
-            'ACL' => 'public-read', // Cela dépend de vos besoins de sécurité
         ]);
-
+    
         return $fileName;
     }
 
@@ -51,9 +49,38 @@ class ImageManager
         ]);
     }
 
-    public function generateImageLink(string $imageName): string
+    public function generateImageLinkk(string $imageName): string
     {
         $imageBaseUrl = $this->parameterBag->get('app.image_base_url');
         return $imageBaseUrl . '/' . $imageName;
+    }
+    public function generateImageLink(string $imageName): string
+    {
+        $bucketName = $this->parameterBag->get('app.s3_bucket');
+        $uploadDirectory = $this->parameterBag->get('app.upload_directory');
+
+        // Configuration du client S3
+        $s3Client = new S3Client([
+            'region' => $this->parameterBag->get('aws_s3_region'),
+            'version' => 'latest',
+            'credentials' => [
+                'key' => $this->parameterBag->get('aws_s3_key'),
+                'secret' => $this->parameterBag->get('aws_s3_secret'),
+            ],
+        ]);
+
+        // Génération de l'URL signée avec une validité de 1 heure (3600 secondes)
+        try {
+            $command = $s3Client->getCommand('GetObject', [
+                'Bucket' => $bucketName,
+                'Key' => $uploadDirectory.'/'.$imageName,
+            ]);
+            $request = $s3Client->createPresignedRequest($command, '+1 hour');
+            $signedUrl = (string)$request->getUri();
+            return $signedUrl;
+        } catch (S3Exception $e) {
+            // Gestion des erreurs, par exemple, le fichier n'existe pas
+            return ''; // ou lancez une exception selon votre besoin
+        }
     }
 }
