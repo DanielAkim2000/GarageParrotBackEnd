92b96cde703b7045db9853e45c6a3fa355667c33
DanielAkim2000 <emanedanielakim@gmail.com>
2023-11-18 17:59:36 +0100

    Utilisation de S3 pour mes images

--- src/Service/ImageManager.php
+++ src/Service/ImageManager.php
@@ -1,7 +1,10 @@
 <?php 
 
+// App\Service\ImageManager.php
+
 namespace App\Service;
 
+use Aws\S3\S3Client;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -9,31 +12,45 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 class ImageManager
 {
     private $parameterBag;
+    private $s3Client;
 
-    public function __construct(ParameterBagInterface $parameterBag)
+    public function __construct(ParameterBagInterface $parameterBag, S3Client $s3Client)
     {
         $this->parameterBag = $parameterBag;
+        $this->s3Client = $s3Client;
     }
 
     public function upload(File $file): string
     {
-        $uploadDir = $this->parameterBag->get('app.upload_directory');
+        $bucketName = $this->parameterBag->get('app.s3_bucket');
+        $uploadDirectory = $this->parameterBag->get('app.upload_directory');
+
         $fileName = uniqid().'.'.$file->guessExtension();
-        $file->move($uploadDir, $fileName);
+        $file->move($uploadDirectory, $fileName);
+
+        // Utiliser le client AWS S3 pour uploader le fichier dans le compartiment S3
+        $this->s3Client->putObject([
+            'Bucket' => $bucketName,
+            'Key' => $uploadDirectory.'/'.$fileName,
+            'Body' => fopen($file->getPathname(), 'r'),
+            'ACL' => 'public-read', // Cela dépend de vos besoins de sécurité
+        ]);
 
         return $fileName;
     }
 
     public function remove(string $fileName): void
     {
-        $uploadDir = $this->parameterBag->get('app.upload_directory');
-        $filePath = $uploadDir . '/' . $fileName;
-        
-        $filesystem = new Filesystem();
-        if ($filesystem->exists($filePath)) {
-            $filesystem->remove($filePath);
-        }
+        $bucketName = $this->parameterBag->get('app.s3_bucket');
+        $uploadDirectory = $this->parameterBag->get('app.upload_directory');
+
+        // Utiliser le client AWS S3 pour supprimer le fichier du compartiment S3
+        $this->s3Client->deleteObject([
+            'Bucket' => $bucketName,
+            'Key' => $uploadDirectory.'/'.$fileName,
+        ]);
     }
+
     public function generateImageLink(string $imageName): string
     {
         $imageBaseUrl = $this->parameterBag->get('app.image_base_url');
